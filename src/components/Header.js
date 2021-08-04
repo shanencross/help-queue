46d8a9e2c1a66f9f953ed0d4d96a1bbcbd9fc52f
Shanen Cross <shanencross@yahoo.com>
2021-08-03 19:40:18 -0700

    Add image of tickets

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
+import ticketsImage from "./../img/tickets-image.jpg";
 
 function Header() {
   return (
-    <h1>Help Queue</h1>
+    <React.Fragment>
+      <h1>Help Queue</h1>
+      <img src={ticketsImage} alt="Tickets" />
+    </React.Fragment>
   );
 }
 
