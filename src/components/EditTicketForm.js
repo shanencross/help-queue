38a6f1f53fef3603ef12bcb1ad6516d564f4b30e
Shanen Cross <shanencross@yahoo.com>
2021-08-23 19:09:23 -0700

    Add edit functionality

---
+++ src/components/EditTicketForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types"
+import ReusableForm from "./ReusableForm";
+
+function EditTicketForm(props) {
+  const { ticket } = props;
+
+  function handleEditTicketFormSubmission(event) {
+    event.preventDefault();
+    props.onEditTicket({
+      names: event.target.names.value,
+      location: event.target.location.value,
+      issue: event.target.issue.value,
+      id: ticket.id
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleEditTicketFormSubmission}
+        buttonText="Update Ticket" />
+    </React.Fragment>
+  )
+}
+
+EditTicketForm.propTypes = {
+  ticket: PropTypes.object,
+  onEditTicket: PropTypes.func
+}
+
+export default EditTicketForm;
--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,32 +1,24 @@
 import React from "react";
+import ReusableForm from "./ReusableForm";
 import { v4 } from "uuid";
 import PropTypes from "prop-types";
 
 function NewTicketForm(props) {
   function handleNewTicketFormSubmission(event) {
     event.preventDefault();
-    props.onNewTicketCreation({names: event.target.names.value, 
-                              location: event.target.location.value, 
-                              issue: event.target.issue.value, 
-                              id: v4()});
+    props.onNewTicketCreation({
+      names: event.target.names.value, 
+      location: event.target.location.value, 
+      issue: event.target.issue.value, 
+      id: v4()
+    });
   }
 
   return (
     <React.Fragment>
-      <form onSubmit={handleNewTicketFormSubmission}>
-        <input
-          type="text"
-          name="names"
-          placeholder="Pair Names" />
-        <input
-          type="text"
-          name="location"
-          placeholder="Location" />
-        <textarea
-          name="issue"
-          placeholder="Describe your issue." />
-        <button type="sumit">Help!</button>
-      </form>
+      <ReusableForm
+      formSubmissionHandler={handleNewTicketFormSubmission}
+      buttonText="Help!"/>
     </React.Fragment>
   );
 }
