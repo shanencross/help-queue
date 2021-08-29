c4eb6e268a61543d9f5092547cd9b19f432d9337
Shanen Cross <shanencross@yahoo.com>
2021-08-29 15:48:48 -0700

    Add and pass test for checking initial state of reducers match root reducer
    Co-authored-by: Cat Denton <cat.denton.dev@gmail.com>
    Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com>

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,8 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import ticketListReducer from '../../reducers/ticket-list-reducer';
+let store = createStore(rootReducer);
 
 describe('rootReducer', () => {
   test('Should return default state if no action type is recognized', () => {
@@ -6,5 +10,13 @@ describe('rootReducer', () => {
       masterTicketList: {},
       formVisibleOnPage: false
     });
+  });
+
+  test('Check that the initial state of ticketListReducer matches root reducer', () => {
+    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, { type: null }));
+  });
+
+  test('Check that the initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
 });
