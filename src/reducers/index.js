d4503486c13d21e360b7dfd01de98e189b377c52
Shanen Cross <shanencross@yahoo.com>
2021-09-19 08:18:57 -0700

    Fix typo importing firestoreReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import formVisibleReducer from './form-visible-reducer';
 import ticketListReducer from './ticket-list-reducer';
-import { fireStoreReducer } from 'redux-firestore'; // brackets?
+import { firestoreReducer } from 'redux-firestore'; // brackets?
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   masterTicketList: ticketListReducer,
   formVisibleOnPage: formVisibleReducer,
-  firestore: fireStoreReducer,
+  firestore: firestoreReducer,
 });
 
 export default rootReducer;
